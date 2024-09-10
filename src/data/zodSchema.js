b90a5b761522a1431453860acaf768e84c148e64
DamodarPatil <patildamodar1983@gmail.com>
2024-09-10 21:54:42 +0530

    Refactor import statements in zodSchema.js and add zodSchema for login and environment variables

--- src/data/zodSchema.js
+++ src/data/zodSchema.js
@@ -1,5 +1,6 @@
 import z from "zod";
 
+// Define schema for user data
 const userSchema = z.object({
   email: z.string().email({ message: "Invalid email address." }),
   password: z
@@ -10,4 +11,21 @@ const userSchema = z.object({
     .min(3, { message: "Name must be at least 3 characters long." }),
 });
 
-export default userSchema;
+// Define schema for login data
+const loginSchema = z.object({
+  email: z.string().email({ message: "Invalid email address." }),
+  password: z
+    .string()
+    .min(8, { message: "Password must be at least 8 characters long." }),
+});
+
+// Define schema for environment variables
+const envSchema = z.object({
+  VITE_APPWRITE_API_ENDPOINT: z.string().url(),
+  VITE_APPWRITE_PROJECT_ID: z.string(),
+  VITE_APPWRITE_DATABASE_ID: z.string(),
+  VITE_APPWRITE_COLLECTION_ID: z.string(),
+  VITE_APPWRITE_BUCKET_ID: z.string(),
+});
+
+export { userSchema, loginSchema, envSchema };
