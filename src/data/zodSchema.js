8e31b73485a72baa4ed084a826567a651f5feee9
DamodarPatil <patildamodar1983@gmail.com>
2024-09-11 15:59:42 +0530

    Refactor import statements in zodSchema.js and add userId validation to createPostSchema

--- src/data/zodSchema.js
+++ src/data/zodSchema.js
@@ -30,12 +30,12 @@ const envSchema = z.object({
 
 // Define schema for post data
 const createPostSchema = z.object({
-    title:z.string().min(1,{message: "Title is required."}),
-    slug:z.string().min(1,{message:"Slug is required."}),
-    content: z.string().min(1,{message: "Content is required."}),
-    featuredImage: z.string().url({message:"Invalid URL for featured image."}),
-    status: z.enum(["draft","published"]).default("draft"),
-})
-
+  title: z.string().min(1, { message: "Title is required." }),
+  slug: z.string().min(1, { message: "Slug is required." }),
+  content: z.string().min(1, { message: "Content is required." }),
+  featuredImage: z.string().url({ message: "Invalid URL for featured image." }),
+  status: z.enum(["draft", "published"]).default("draft"),
+  userId: z.string().min(1, { message: "User ID is required." }),
+});
 
 export { userSchema, loginSchema, envSchema, createPostSchema };
