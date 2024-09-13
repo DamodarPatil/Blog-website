69753274c9b15a4737f6af7c1b1080d45aeb9c97
DamodarPatil <patildamodar1983@gmail.com>
2024-09-13 20:14:55 +0530

    Refactor useCurrentUser hook to remove unused code, improve error handling, and handle authentication and loading state

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,35 @@
-import { useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
-import authService from "./appwrite/authService";
-import { login, logout } from "./store/authSlice";
 import { Header, Footer } from "./components/index";
-import { logger } from "./data/logger";
-// import { Outlet } from "react-router-dom";
+import useCurrentUser from "./hooks/useCurrentUser";
 
 const App = () => {
-  const [loading, setLoading] = useState(true);
-  const dispatch = useDispatch();
+  const { loading, error } = useCurrentUser();
 
-  useEffect(() => {
-    const controller = new AbortController(); // Create an AboutController instance
-    const { signal } = controller;
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center min-h-screen bg-gray-100">
+        <p className="text-xl font-semibold">Loading...</p>
+      </div>
+    );
+  }
 
-    const fetchCurrentUser = async () => {
-      try {
-        const userData = await authService.getCurrentUser();
-        if (!signal.aborted) {
-          //Check if the operation was aborted
-          if (userData) {
-            dispatch(login({ userData }));
-          } else {
-            dispatch(logout());
-          }
-        }
-      } catch (error) {
-        if (!signal.aborted) {
-          logger.error("Failed to fetch current user:", {
-            message: error.message,
-          });
-          dispatch(logout());
-        }
-      } finally {
-        if (!signal.aborted) {
-          setLoading(false);
-        }
-      }
-    };
-    fetchCurrentUser();
+  if (error) {
+    return (
+      <div className="flex justify-center items-center min-h-screen bg-gray-100">
+        {/* Display a generic error message */}
+        <p className="text-xl font-semibold text-red-500">{error}</p>
+      </div>
+    );
+  }
 
-    return () => {
-      controller.abort(); //Abort the fetch operation on component unmount
-    };
-  }, [dispatch]);
-
-  return !loading ? (
+  return (
     <div className="min-h-screen flex flex-col justify-between bg-gray-100">
       <div className="w-full">
         <Header />
         <main className="flex-grow p-4 md:p-8">
-          {" "}
-          {/* Added padding for better spacing */}
-          TODO: {/* <Outlet/> */}
+          {/* Placeholder for future routes */}
         </main>
         <Footer />
       </div>
-    </div>
-  ) : (
-    <div className="flex justify-center items-center min-h-screen bg-gray-100">
-      <p className="text-xl font-semibold">Loading...</p>
     </div>
   );
 };
