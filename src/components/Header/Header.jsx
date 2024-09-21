e727f01edb29fb8de0e4278b3b622ec324498520
DamodarPatil <patildamodar1983@gmail.com>
2024-09-21 09:52:21 +0530

    Refactor Header component to import Logo, LogoutBtn, and Container from the correct locations and update the layout

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,64 @@
+import { Container, LogoutBtn, Logo } from "../index";
+import { useSelector } from "react-redux";
+import { useMemo } from "react";
+import NavItems from "./NavItems";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+  const authStatus = useSelector((state) => state.auth.status);
 
-export default Header
+  const navItems = useMemo(
+    () => [
+      {
+        name: "Home",
+        slug: "/",
+        active: true,
+      },
+      {
+        name: "Login",
+        slug: "/login",
+        active: !authStatus,
+      },
+      {
+        name: "Signup",
+        slug: "/signup",
+        active: !authStatus,
+      },
+      {
+        name: "All Posts",
+        slug: "/all-posts",
+        active: authStatus,
+      },
+      {
+        name: "Add Post",
+        slug: "/add-post",
+        active: authStatus,
+      },
+    ],
+    [authStatus]
+  );
+
+  return (
+    <header>
+      <Container>
+        <nav className="">
+          <div className="">
+            <Link to="/">
+              <Logo width="70px" />
+            </Link>
+          </div>
+          <ul className="flex ml-auto">
+            <NavItems items={navItems} />
+            {authStatus && (
+              <li>
+                <LogoutBtn />
+              </li>
+            )}
+          </ul>
+        </nav>
+      </Container>
+    </header>
+  );
+};
+
+export default Header;
