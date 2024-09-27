fe71aa6b425344340b8fc3de8c50d6cdb97c91c7
DamodarPatil <patildamodar1983@gmail.com>
2024-09-27 12:25:05 +0530

    Refactor Input component to update default background, text, and border colors

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,36 +1,31 @@
 import { forwardRef, useId } from "react";
 import PropTypes from "prop-types";
 
-const Input = forwardRef(
-  ({ label, type = "text", className = "", ...props }, ref) => {
-    const id = useId();
-    return (
-      <div className="w-full">
-        {label && (
-          <label className="inline-block mb-1 pl-1" htmlFor={id}>
-            {label}
-          </label>
-        )}
-        <input
-          type={type}
-          className={`px-2 py-2 rounded-lg bg-[#FFF4EA] outline-none focus:bg-[#FADFA1]
+const Input = ({ label, type = "text", className = "", ...props }, ref) => {
+  const id = useId();
+  return (
+    <div className="w-full">
+      {label && (
+        <label className="inline-block mb-1 pl-1" htmlFor={id}>
+          {label}
+        </label>
+      )}
+      <input
+        type={type}
+        className={`px-2 py-2 rounded-lg bg-[#FFF4EA] outline-none focus:bg-[#FADFA1]
             duration-200 border border-[#C96868] w-full ${className}`}
-          ref={ref}
-          {...props}
-          id={id}
-        />
-      </div>
-    );
-  }
-);
-
-Input.displayName = "Input";
+        ref={ref}
+        {...props}
+        id={id}
+      />
+    </div>
+  );
+};
 
 Input.propTypes = {
   label: PropTypes.string,
   type: PropTypes.string,
   className: PropTypes.string,
-  props: PropTypes.object,
 };
 
-export default Input;
+export default forwardRef(Input);
