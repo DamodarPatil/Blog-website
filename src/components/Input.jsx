02edccdcf3944cf3777848c4dc84ee6d016affd9
DamodarPatil <patildamodar1983@gmail.com>
2024-09-27 11:04:21 +0530

    Refactor Input component to update default background, text, and border colors

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -13,7 +13,7 @@ const Input = forwardRef(
         )}
         <input
           type={type}
-          className={`px-2 py-2 rounded-lg bg-white outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
+          className={`px-2 py-2 rounded-lg bg-[#FFF4EA] outline-none focus:bg-[#FADFA1] duration-200 border border-[#C96868] w-full ${className}`}
           ref={ref}
           {...props}
           id={id}
@@ -31,6 +31,5 @@ Input.propTypes = {
   className: PropTypes.string,
   props: PropTypes.object,
 };
-
 
 export default Input;
