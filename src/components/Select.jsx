0d442711beac9c4b14cc4b657227b00a74a64abd
DamodarPatil <patildamodar1983@gmail.com>
2024-09-27 12:25:10 +0530

    Refactor Select component to update styles and add prop types

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -1,13 +1,37 @@
-import { forwardRef, useId } from "react"
+import { forwardRef, useId } from "react";
+import PropTypes from "prop-types";
 
-const Select = ({
-    options,
-    label,
-    className
-}) => {
+const Select = ({ options, label, className = "", ...props }, ref) => {
+  const id = useId();
+
   return (
-    <div>Select</div>
-  )
-}
+    <div className="w-full">
+      {label && (
+        <label htmlFor={id} className="">
+          {label}
+        </label>
+      )}
+      <select
+        {...props}
+        id={id}
+        ref={ref}
+        className={`px-2 py-2 rounded-lg bg-[#FFF4EA] outline-none focus:bg-[#FADFA1]
+            duration-200 border border-[#C96868] w-full ${className}`}
+      >
+        {options?.map((index, optionValue) => (
+          <option key={index} value={optionValue}>
+            {optionValue}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
 
-export default Select
+Select.propTypes = {
+  options: PropTypes.arrayOf(PropTypes.string).isRequired,
+  label: PropTypes.string,
+  className: PropTypes.string,
+};
+
+export default forwardRef(Select);
