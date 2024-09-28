522d16b88e72593e6f13efdfe8e735c14a58c845
DamodarPatil <patildamodar1983@gmail.com>
2024-09-28 15:00:43 +0530

    Refactor Signup component to include form validation using @hookform/resolvers and add Button and Input components

--- src/components/Auth/Signup.jsx
+++ src/components/Auth/Signup.jsx
@@ -1,10 +1,92 @@
-import { useState } from "react"
-import authService from "../../appwrite/authService"
+import { useState } from "react";
+import authService from "../../appwrite/authService";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button, Input, Logo } from "../index";
+import { userSchema } from "../../data/zodSchema";
+import { useForm } from "react-hook-form";
+import { useDispatch } from "react-redux";
+import { login as registerUser } from "../../store/authSlice";
+import { Link, useNavigate } from "react-router-dom";
 
 const Signup = () => {
-  return (
-    <div>Signup</div>
-  )
-}
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { register, handleSubmit } = useForm({
+    resolver: zodResolver(userSchema),
+  });
+  const [error, setError] = useState();
+  const [loading, setLoading] = useState(false);
 
-export default Signup
+  const createAccount = async (data) => {
+    setError("");
+    setLoading(true);
+    try {
+      const createdUserData = await authService.createAccount(data);
+      if (createdUserData) {
+        const userData = await authService.getCurrentUser();
+        if (userData) {
+          dispatch(registerUser(userData));
+          navigate("/");
+        }
+      }
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="">
+      <div className="">
+        <div className="mb-2 flex justify-center">
+          <span className="inline-block w-full max-w-[100px]">
+            <Logo width="100%" />
+          </span>
+        </div>
+        <h2 className="text-center text-2xl font-bold leading-tight">
+          Sign up to your account
+        </h2>
+        <p className="mt-2 text-center text-base text-black/60">
+          Already have an account?&nbsp;
+          <Link
+            to="/login"
+            className="font-medium text-primary transition-all duration-200 hover:underline"
+          >
+            Log In
+          </Link>
+        </p>
+        {error && (
+          <p className="text-red-600 mt-8 text-center">{error.message}</p>
+        )}
+        <form onSubmit={handleSubmit(createAccount)}>
+          <div className="space-y-5">
+            <Input
+              label="Name: "
+              placeholder="Enter your name"
+              type="text"
+              {...register("name")}
+            />
+            <Input
+              label="Email: "
+              placeholder="Enter your email"
+              type="email"
+              {...register("email")}
+            />
+            <Input
+              label="Password: "
+              placeholder="Enter your password"
+              type="password"
+              {...register("password")}
+            />
+            <Button type="submit" className="w-full" disabled={loading}>
+              {loading ? "Signing up..." : "Sign up"}
+            </Button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
